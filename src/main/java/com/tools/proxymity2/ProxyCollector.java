81b74657180082a12ad92cbcf2586e719d005c3a
nikosglikis@gmail.com <nikosglikis@gmail.com>
2016-01-15 19:29:03 +0200

    Added Lots of new sources.

---
+++ src/main/java/com/tools/proxymity2/ProxyCollector.java
@@ -0,0 +1,195 @@
+package com.tools.proxymity;
+
+import com.tools.proxymity.DataTypes.CollectorParameters;
+import com.toortools.os.OsHelper;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Random;
+import java.util.Vector;
+import java.util.logging.Level;
+
+abstract public class ProxyCollector extends  Thread
+{
+    private Vector<ProxyInfo> proxies = new Vector<ProxyInfo>();
+
+    protected Connection dbConnection;
+    protected PhantomJSDriver driver;
+    protected boolean useTor = false;
+    public ProxyCollector(CollectorParameters collectorParameters)
+    {
+        //this.collectorParameters = collectorParameters;
+        this.dbConnection = collectorParameters.getDbConnection();
+        this.useTor = collectorParameters.isUseTor();
+    }
+
+    public ProxyCollector() throws Exception
+    {
+        throw new Exception("Default controller not allowed");
+    }
+    public abstract Vector<ProxyInfo> collectProxies();
+
+    final int SLEEP_SECONDS_BETWEEN_SCANS = 30;
+
+
+    public void run()
+    {
+        while (true)
+        {
+
+            try
+            {
+                Thread.sleep(new Random().nextInt(5000));
+                initProxies();
+                Vector<ProxyInfo> proxyInfos = collectProxies();
+
+                writeProxyInfoToDatabase(proxyInfos);
+
+                Thread.sleep(SLEEP_SECONDS_BETWEEN_SCANS * 1000);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private synchronized void initProxies()
+    {
+        proxies  = new Vector<ProxyInfo>();
+    }
+
+    protected synchronized void addProxy(ProxyInfo proxyInfo)
+    {
+        proxies.add(proxyInfo);
+    }
+
+    protected Vector<ProxyInfo> getProxies()
+    {
+        return proxies;
+    }
+
+    public synchronized void writeProxyInfoToDatabase()
+    {
+        this.writeProxyInfoToDatabase(this.proxies);
+    }
+
+    private void writeProxyInfoToDatabase(Vector<ProxyInfo> proxyInfos)
+    {
+        try
+        {
+            for (ProxyInfo proxyInfo : proxyInfos)
+            {
+                if (
+                        proxyInfo.getHost() == null
+                        || proxyInfo.getPort() == null
+                        || proxyInfo.getType() == null
+
+                        )
+                {
+                    continue;
+                }
+                String query = "INSERT INTO `proxies`.`"+Proxymity.TABLE_NAME+"` (" +
+                        "`id`, " +
+                        "`host`, " +
+                        "`port`, " +
+                        "`type`, " +
+                        "`inserted`, " +
+                        "`lastchecked`, " +
+                        "`status`, " +
+                        "`fullanonymous` " +
+
+                        ") VALUES  (" +
+                        "0, " +
+                        "'"+sanitizeDatabaseInput(proxyInfo.getHost())+"', " +
+                        "'"+sanitizeDatabaseInput(proxyInfo.getPort())+"', " +
+                        "'"+sanitizeDatabaseInput(proxyInfo.getType())+"', " +
+                        "NOW(), " +
+                        "NULL, " +
+                        "'pending', " +
+                        "'no')";
+                //System.out.println(query);
+
+                Statement st = dbConnection.createStatement();
+                try
+                {
+                    st.executeUpdate(query);
+                }
+                catch (Exception MySQLIntegrityConstraintViolationException)
+                {
+
+                }
+                st.close();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    String sanitizeDatabaseInput(String value)
+    {
+        while (value.contains("''")) {
+            value = value.replace("''","'");
+        }
+        return value.replace("'","''");
+    }
+
+    public void initializePhantom()
+    {
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
+        //Utilities.readUrl("http://proxylist.hidemyass.com/2#listable");
+
+        Capabilities caps = new DesiredCapabilities();
+        String[] phantomArgs = new String[] {
+
+        };
+        if (!useTor)
+        {
+            phantomArgs = new  String[] {
+                    "--webdriver-loglevel=NONE"
+            };
+        }
+        else
+        {
+            phantomArgs = new  String[] {
+                    "--webdriver-loglevel=NONE",
+                    "--proxy=127.0.0.1:9050",
+                    "--proxy-type=socks5"
+            };
+        }
+
+
+
+        if (OsHelper.isWindows())
+        {
+            ((DesiredCapabilities) caps).setJavascriptEnabled(true);
+            ((DesiredCapabilities) caps).setJavascriptEnabled(true);
+            ((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
+            ((DesiredCapabilities) caps).setCapability(
+                    PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                    "bin\\phantomjs.exe"
+            );
+            ((DesiredCapabilities) caps).setCapability(
+                    PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomArgs
+            );
+        }
+        else
+        {
+            ((DesiredCapabilities) caps).setJavascriptEnabled(true);
+            ((DesiredCapabilities) caps).setCapability("takesScreenshot", true);
+            ((DesiredCapabilities) caps).setCapability(
+                    PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomArgs
+            );
+        }
+        driver = new PhantomJSDriver(caps);
+
+    }
+}
